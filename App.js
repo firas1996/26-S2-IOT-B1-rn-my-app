93848255adbd636bd0c4a3eda970d9943fbfce15
firas1996 <firas.hm.1996@gmail.com>
2026-04-16 10:41:06 +0100

    aa

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import AbcTest from "./src/components/AbcTest";
 
 export default function App() {
+  const name = "Firas";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, .... !!!</Text>
+      <AbcTest name={name} />
+      <Button title="Click me !" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/components/AbcTest.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const AbcTest = ({ name }) => {
+  const test = "World";
+  return <Text>Welcome {name} !</Text>;
+};
+export default AbcTest;
